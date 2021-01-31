5bddec6e5e358a2c39abf9fce6ee3d5235f6acfe
ssq <Skyleba@ukr.net>
2021-01-31 03:44:01 +0200

    sub

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const icons = document.querySelectorAll('.question-icon');
+const answers = document.querySelectorAll('.question-answer')
+
+
+for(let i = 0; i < icons.length; i++) {
+  answers[i].style.display = 'none';
+  icons[i].addEventListener('click', (e) =>{
+    console.log(answers[i].style.display);
+    if (answers[i].style.display === 'none') {
+      answers[i].style.display = 'block';
+      icons[i].classList.add('active');
+    } else {
+      answers[i].style.display = 'none';
+      icons[i].classList.remove('active');
+    }
+  })
+}
