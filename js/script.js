861eabcd2620cb0e8f03f394bd98b79617c9eb29
ssq <Skyleba@ukr.net>
2021-01-31 04:27:39 +0200

    sub1

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ for(let i = 0; i < icons.length; i++) {
     console.log(answers[i].style.display);
     if (answers[i].style.display === 'none') {
       answers[i].style.display = 'block';
+      answers[i].style.top = '100%';
       icons[i].classList.add('active');
     } else {
       answers[i].style.display = 'none';
